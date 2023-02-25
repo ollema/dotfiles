579ec5f867c5a6c7d097c790be9be5dd6f915d69
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-02-25 15:33:43 +0100

    update prompt

--- home/default.nix
+++ home/default.nix
@@ -5,15 +5,17 @@
   # packages
   # ---------------------------------------------------------------------------
   home.packages = with pkgs; [
+    # git
     hub # git wrapper
-    wget # download files
-    fzf # fuzzy finder
+
+    # utils
     bat # fancy version of `cat`
-    fd # fancy version of `find`
+    duf # fancy version of `df`
     exa # fancy version of `ls`
+    fd # fancy version of `find`
+    fzf # fuzzy finder
     htop # fancy version of `top`
     ripgrep # fancy version of `grep`
-    duf # fancy version of `df`
     tealdeer # rust implementation of `tldr`
   ];
 
@@ -41,9 +43,6 @@
   # ---------------------------------------------------------------------------
   # shell
   # ---------------------------------------------------------------------------
-  # starship prompt
-  programs.starship = { enable = true; };
-
   # fish shell
   programs.fish = {
     enable = true;
@@ -64,7 +63,7 @@
       gc = "git commit";
       gcm = "git commit -m";
       gca = "git commit --amend";
-      gac = "git add .; git commit --amend";
+      gac = "git add .; git commit --amend --no-edit";
       gp = "git push";
       gl = "git l";
       gd = "git diff";
@@ -72,8 +71,55 @@
     };
 
     interactiveShellInit = "starship init fish | source";
-
     functions.fish_greeting = "";
+  };
+
+  # starship prompt
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+
+      directory = { style = "blue"; };
+
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
+
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_state = {
+        format = "([$state( $progress_current/$progress_total)]($style)) ";
+        style = "bright-black";
+      };
+
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
+      };
+    };
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
